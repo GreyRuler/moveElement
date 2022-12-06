2b54a362db7a7e74f4ec79794b4170ee4b313991
GreyRuler <daniil_vasilenko757@mail.ru>
2022-12-06 23:04:43 +0300

    Impl new feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './css/style.css';
 import './ts/app.ts';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
 // Точка входа webpack
 // Не пишите код в данном файле
--- src/ts/GamePlay.ts
+++ src/ts/GamePlay.ts
@@ -1,31 +1,63 @@
+import { Toast } from 'bootstrap';
+
 export default class GamePlay {
 	private readonly goblin: HTMLElement;
 
 	private container: Element;
 
 	private counter: number;
+
 	private counterElement: HTMLElement | null;
+
+	private readonly cells: NodeListOf<Element>;
+
+	private miss: number;
+
+	private timer: NodeJS.Timer | undefined;
+
+	private toast: Toast;
+
+	private score: HTMLElement | null;
 
 	constructor(container: Element) {
 		this.container = container;
 		this.counter = 0;
+		this.miss = 0;
 		this.goblin = this.createGoblin();
 		this.counterElement = document.querySelector('.counter');
+		this.toast = new Toast(
+			document.querySelector('.toast')!,
+			{
+				delay: 1000000000
+			}
+		);
+		this.score = document.querySelector('.score');
+		this.cells = this.container.querySelectorAll('.box');
 	}
 
 	init() {
 		const timeout = 1000;
-		setInterval(this.moveGoblin, timeout);
+		this.timer = setInterval(this.moveGoblin.bind(this), timeout);
 		this.goblin.addEventListener('click', () => {
 			this.goblin.classList.add('d-none');
 			this.counter++;
 			this.counterElement!.textContent = String(this.counter);
 		});
+		this.cells.forEach((cell) => {
+			cell.addEventListener('click', (event: Event) => {
+				const target = event.target as HTMLElement;
+				if (!target?.closest('.goblin')) {
+					this.miss++;
+					if (this.miss === 5) {
+						this.endGame();
+					}
+				}
+			});
+		});
 	}
 
 	moveGoblin() {
-		const cells = this.container.querySelectorAll('.box');
-		const cell: HTMLElement = this.randomElementFromArray(Array.from(cells));
+		const cell: HTMLElement = this.randomElementFromArray(Array.from(this.cells));
 		this.goblin.classList.remove('d-none');
 		cell.insertAdjacentElement('afterbegin', this.goblin);
 	}
@@ -36,9 +68,17 @@ export default class GamePlay {
 		return array[randomIndex];
 	}
 
+	// eslint-disable-next-line class-methods-use-this
 	createGoblin() {
 		const goblin = document.createElement('div');
 		goblin.classList.add('goblin');
 		return goblin;
+	}
+
+	endGame() {
+		clearInterval(this.timer);
+		this.goblin.classList.add('d-none');
+		this.score!.textContent = String(this.counter);
+		this.toast.show();
 	}
 }
